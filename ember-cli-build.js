75e6420d4880318598f013263d212328a843a990
Christiaan Quyn <christianq010@gmail.com>
2017-03-30 18:39:49 +0530

    Feat: Add nav links to index page, Route to abous us

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,7 @@ module.exports = function(defaults) {
     }
   });
 
-  // Use `app.import` to add additional libraries to the generated
+    // Use `app.import` to add additional libraries to the generated
   // output files.
   //
   // If you need to use different assets in different
@@ -23,5 +23,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+
+
+    return app.toTree();
 };
