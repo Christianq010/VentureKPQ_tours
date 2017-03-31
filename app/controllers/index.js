16f1eb68630d0039824995b47ddd6ba27be4f89c
Christiaan Quyn <christianq010@gmail.com>
2017-03-31 16:28:46 +0530

    feat: Add admin/subscriptions page via route,  Return database values to Admin/Subscriptions page
    response message for succesful email sub,data tabulated in admin/sub Page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,11 +14,14 @@ export default Ember.Controller.extend({
         saveEmailAddress() {
             const email = this.get('emailAddress');
 
-            const newSubscription = this.store.createRecord('newsletter', { email: email });
+            const newSubscription = this.store.createRecord('newsletter', {
+                email: email });
             newSubscription.save();
 
-            this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-            this.set('emailAddress', '');
+            newSubscription.save().then((response) => {
+                this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+                this.set('emailAddress', '');
+            });
         }
     }
 
--- app/router.js
+++ app/router.js
@@ -9,6 +9,10 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('menu');
   this.route('about');
+
+  this.route('admin', function() {
+    this.route('subscriptions');
+  });
 });
 
 export default Router;
